06e9be70b49f9b51e41a25846dfecf192bf1bc58
cozyBoi <kjjh1126@sogang.ac.kr>
2022-09-28 14:17:28 +0900

    attach mongo db

--- server/main.go
+++ server/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var mg_client *mongo.Client
 
 var upgrader = websocket.Upgrader{
 	EnableCompression: true,
@@ -64,8 +72,11 @@ func socketHandler(ctx echo.Context) error {
 		if chatCache[roomId].IsFull() {
 			chatCache[roomId].Pop()
 		}
-		newChat := buff{Msg: string(msg), Sender: cid.Value}
-		fmt.Println(newChat)
+		str_msg := string(msg)
+		newChat := buff{Msg: str_msg, Sender: cid.Value}
+		coll := mg_client.Database("go-chat-server").Collection("chat-info")
+		docs := bson.D{{"room", roomId}, {"id", cid.Value}, {"msg", str_msg}}
+		coll.InsertOne(context.TODO(), docs)
 		chatCache[roomId].Push(newChat)
 		broadcast_msg(c, msg, roomId)
 	}
@@ -118,6 +129,23 @@ func roomsCreate(ctx echo.Context) error {
 func main() {
 	conns = make(map[int][]*websocket.Conn)
 	chatCache = make(map[int]*queue)
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	mg_client = client
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
 
 	e := echo.New()
 
